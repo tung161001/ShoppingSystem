89ebdc43ea11878f269fc5ca63d5653d50aeafb1
tung161001 <106803739+tung161001@users.noreply.github.com>
2023-08-14 11:54:49 +0700

    Create FStoreDB.sql
    upload database

---
+++ FStoreDB.sql
@@ -0,0 +1,252 @@
+﻿USE [master]
+GO
+/****** Object:  Database [FStore]    Script Date: 01/07/2022 1:24:00 am ******/
+CREATE DATABASE [FStore]
+
+ALTER DATABASE [FStore] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [FStore].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [FStore] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [FStore] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [FStore] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [FStore] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [FStore] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [FStore] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [FStore] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [FStore] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [FStore] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [FStore] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [FStore] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [FStore] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [FStore] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [FStore] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [FStore] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [FStore] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [FStore] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [FStore] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [FStore] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [FStore] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [FStore] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [FStore] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [FStore] SET RECOVERY FULL 
+GO
+ALTER DATABASE [FStore] SET  MULTI_USER 
+GO
+ALTER DATABASE [FStore] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [FStore] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [FStore] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [FStore] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [FStore] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [FStore] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'FStore', N'ON'
+GO
+ALTER DATABASE [FStore] SET QUERY_STORE = OFF
+GO
+USE [FStore]
+GO
+/****** Object:  Table [dbo].[Category]    Script Date: 01/07/2022 1:24:01 am ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Category](
+	[CategoryID] [int] NOT NULL,
+	[CategoryName] [nvarchar](20) NOT NULL,
+ CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED 
+(
+	[CategoryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Member]    Script Date: 01/07/2022 1:24:01 am ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Member](
+	[MemberID] [int] NOT NULL,
+	[Email] [varchar](100) NOT NULL,
+	[CompanyName] [varchar](40) NOT NULL,
+	[City] [varchar](15) NOT NULL,
+	[Country] [varchar](15) NOT NULL,
+	[Password] [varchar](30) NOT NULL,
+ CONSTRAINT [PK_Member] PRIMARY KEY CLUSTERED 
+(
+	[MemberID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Order]    Script Date: 01/07/2022 1:24:01 am ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Order](
+	[OrderID] [int] NOT NULL,
+	[MemberID] [int] NOT NULL,
+	[OrderDate] [datetime] NOT NULL,
+	[RequiredDate] [datetime] NULL,
+	[ShipppedDate] [datetime] NULL,
+	[Freight] [money] NULL,
+ CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED 
+(
+	[OrderID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[OrderDetail]    Script Date: 01/07/2022 1:24:01 am ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OrderDetail](
+	[OrderID] [int] NOT NULL,
+	[ProductID] [int] NOT NULL,
+	[UnitPrice] [money] NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[Discount] [float] NOT NULL,
+ CONSTRAINT [PK_OrderDetail] PRIMARY KEY CLUSTERED 
+(
+	[OrderID] ASC,
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Product]    Script Date: 01/07/2022 1:24:01 am ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Product](
+	[ProductID] [int] NOT NULL,
+	[CategoryID] [int] NOT NULL,
+	[ProductName] [varchar](40) NOT NULL,
+	[Weight] [varchar](20) NOT NULL,
+	[UnitPrice] [money] NOT NULL,
+	[UnitsInStock] [int] NOT NULL,
+ CONSTRAINT [PK_Table_1] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+INSERT [dbo].[Category] ([CategoryID], [CategoryName]) VALUES (1, N'Fruit')
+INSERT [dbo].[Category] ([CategoryID], [CategoryName]) VALUES (2, N'Vegetable')
+INSERT [dbo].[Category] ([CategoryID], [CategoryName]) VALUES (3, N'Meat')
+GO
+INSERT [dbo].[Member] ([MemberID], [Email], [CompanyName], [City], [Country], [Password]) VALUES (1, N'quochda@gmail.com', N'123', N'New York', N'America', N'quochda')
+INSERT [dbo].[Member] ([MemberID], [Email], [CompanyName], [City], [Country], [Password]) VALUES (2, N'annn@gmail.com', N'123', N'Singapore', N'Singapore', N'annn')
+INSERT [dbo].[Member] ([MemberID], [Email], [CompanyName], [City], [Country], [Password]) VALUES (3, N'sontpk@gmail.com', N'456', N'Beijing', N'China', N'sontpk')
+INSERT [dbo].[Member] ([MemberID], [Email], [CompanyName], [City], [Country], [Password]) VALUES (4, N'datnq@gmail.com', N'456', N'London', N'England', N'datnq')
+INSERT [dbo].[Member] ([MemberID], [Email], [CompanyName], [City], [Country], [Password]) VALUES (5, N'dongbh@gmail.com', N'789', N'Moscow', N'Russia', N'dongbh')
+INSERT [dbo].[Member] ([MemberID], [Email], [CompanyName], [City], [Country], [Password]) VALUES (6, N'vylt@gmail.com', N'789', N'Toronto', N'Canada', N'vylt')
+INSERT [dbo].[Member] ([MemberID], [Email], [CompanyName], [City], [Country], [Password]) VALUES (7, N'phuonglhk@gmail.com', N'456789', N'Ha Noi', N'Vietnam', N'phuonglhk')
+INSERT [dbo].[Member] ([MemberID], [Email], [CompanyName], [City], [Country], [Password]) VALUES (8, N'nhandt@gmail.com', N'456789', N'London', N'England', N'nhandt')
+GO
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (1, 1, CAST(N'2022-06-27T00:00:00.000' AS DateTime), CAST(N'2022-06-29T00:00:00.000' AS DateTime), CAST(N'2022-06-30T00:00:00.000' AS DateTime), 15000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (2, 5, CAST(N'2022-01-31T00:00:00.000' AS DateTime), CAST(N'2022-02-28T00:00:00.000' AS DateTime), CAST(N'2022-03-01T00:00:00.000' AS DateTime), 30000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (3, 2, CAST(N'2022-02-20T00:00:00.000' AS DateTime), CAST(N'2022-03-01T00:00:00.000' AS DateTime), CAST(N'2022-03-03T00:00:00.000' AS DateTime), 35000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (4, 6, CAST(N'2020-12-31T00:00:00.000' AS DateTime), CAST(N'2021-12-31T00:00:00.000' AS DateTime), CAST(N'2022-12-31T00:00:00.000' AS DateTime), 45000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (5, 8, CAST(N'2021-12-01T00:00:00.000' AS DateTime), CAST(N'2022-01-01T00:00:00.000' AS DateTime), CAST(N'2022-02-01T00:00:00.000' AS DateTime), 25000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (6, 7, CAST(N'2022-01-01T00:00:00.000' AS DateTime), CAST(N'2022-01-02T00:00:00.000' AS DateTime), CAST(N'2022-01-03T00:00:00.000' AS DateTime), 60000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (7, 3, CAST(N'2019-06-29T00:00:00.000' AS DateTime), CAST(N'2020-04-01T00:00:00.000' AS DateTime), CAST(N'2020-04-02T00:00:00.000' AS DateTime), 50000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (8, 1, CAST(N'2020-02-28T00:00:00.000' AS DateTime), CAST(N'2020-02-29T00:00:00.000' AS DateTime), CAST(N'2020-03-01T00:00:00.000' AS DateTime), 55000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (9, 4, CAST(N'2022-06-29T00:00:00.000' AS DateTime), CAST(N'2022-07-03T00:00:00.000' AS DateTime), CAST(N'2022-07-05T00:00:00.000' AS DateTime), 15000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (10, 1, CAST(N'2022-06-29T00:00:00.000' AS DateTime), CAST(N'2022-06-30T00:00:00.000' AS DateTime), CAST(N'2022-01-07T00:00:00.000' AS DateTime), 40000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (11, 6, CAST(N'2019-12-31T00:00:00.000' AS DateTime), CAST(N'2020-01-01T00:00:00.000' AS DateTime), CAST(N'2020-01-02T00:00:00.000' AS DateTime), 51000.0000)
+INSERT [dbo].[Order] ([OrderID], [MemberID], [OrderDate], [RequiredDate], [ShipppedDate], [Freight]) VALUES (12, 3, CAST(N'2021-12-01T00:00:00.000' AS DateTime), CAST(N'2021-12-02T00:00:00.000' AS DateTime), CAST(N'2021-12-03T00:00:00.000' AS DateTime), 63000.0000)
+GO
+INSERT [dbo].[OrderDetail] ([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount]) VALUES (1, 4, 15000.0000, 4, 3)
+INSERT [dbo].[OrderDetail] ([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount]) VALUES (1, 8, 15000.0000, 2, 1)
+INSERT [dbo].[OrderDetail] ([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount]) VALUES (2, 3, 15000.0000, 1, 3)
+INSERT [dbo].[OrderDetail] ([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount]) VALUES (3, 6, 30000.0000, 1, 1)
+INSERT [dbo].[OrderDetail] ([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount]) VALUES (3, 9, 20000.0000, 3, 1)
+INSERT [dbo].[OrderDetail] ([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount]) VALUES (5, 5, 15000.0000, 5, 2)
+INSERT [dbo].[OrderDetail] ([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount]) VALUES (6, 12, 28000.0000, 2, 2)
+INSERT [dbo].[OrderDetail] ([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount]) VALUES (6, 14, 14000.0000, 6, 1)
+INSERT [dbo].[OrderDetail] ([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount]) VALUES (7, 7, 60000.0000, 2, 3)
+INSERT [dbo].[OrderDetail] ([OrderID], [ProductID], [UnitPrice], [Quantity], [Discount]) VALUES (9, 10, 15000.0000, 1, 3)
+GO
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (1, 1, N'Apple', N'1kg', 10000.0000, 3)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (2, 1, N'Banana', N'1.5kg', 20000.0000, 1)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (3, 1, N'Orange', N'0.5kg', 25000.0000, 4)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (4, 1, N'Grape', N'1kg', 15000.0000, 10)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (5, 1, N'Melon', N'3kg', 40000.0000, 2)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (6, 1, N'Pineapple', N'2kg', 30000.0000, 7)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (7, 1, N'Durian', N'4kg', 60000.0000, 5)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (8, 2, N'Carrot', N'0.25kg', 10000.0000, 6)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (9, 3, N'Chicken', N'1.25kg', 20000.0000, 9)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (10, 2, N'Tomato', N'0.25kg', 15000.0000, 2)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (11, 2, N'Eggplant', N'1kg', 35000.0000, 5)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (12, 3, N'Beef', N'2.5kg', 28000.0000, 3)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (13, 3, N'Pork', N'2kg', 21000.0000, 7)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (14, 2, N'Onion', N'0.75kg', 14000.0000, 12)
+INSERT [dbo].[Product] ([ProductID], [CategoryID], [ProductName], [Weight], [UnitPrice], [UnitsInStock]) VALUES (15, 3, N'Lamb', N'5kg', 55000.0000, 8)
+GO
+ALTER TABLE [dbo].[Order]  WITH CHECK ADD  CONSTRAINT [FK_Order_Member] FOREIGN KEY([MemberID])
+REFERENCES [dbo].[Member] ([MemberID])
+GO
+ALTER TABLE [dbo].[Order] CHECK CONSTRAINT [FK_Order_Member]
+GO
+ALTER TABLE [dbo].[OrderDetail]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetail_Order] FOREIGN KEY([OrderID])
+REFERENCES [dbo].[Order] ([OrderID])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[OrderDetail] CHECK CONSTRAINT [FK_OrderDetail_Order]
+GO
+ALTER TABLE [dbo].[OrderDetail]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetail_Product] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Product] ([ProductID])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[OrderDetail] CHECK CONSTRAINT [FK_OrderDetail_Product]
+GO
+ALTER TABLE [dbo].[Product]  WITH CHECK ADD  CONSTRAINT [FK_Category_Product] FOREIGN KEY([CategoryID])
+REFERENCES [dbo].[Category] ([CategoryID])
+GO
+ALTER TABLE [dbo].[Product] CHECK CONSTRAINT [FK_Category_Product]
+GO
+USE [master]
+GO
+ALTER DATABASE [FStore] SET  READ_WRITE 
+GO
+
+
+
+use[FStore]
+
+
+select * from [Member]
